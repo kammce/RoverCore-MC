144e0d525eae7ba18783c008eaf4f94c19e6ba4b
Laptop <talk2bk@hotmail.com>
2016-12-02 13:08:16 -0800

    added the ability to place markers on the map

--- mission-control-2016/Captain/mapScript.js
+++ mission-control-2016/Captain/mapScript.js
@@ -6,21 +6,25 @@ TODO:
 
 var latitude = 37.335083;
 var longitude = -121.882088;
+var altitude = 0;
 document.getElementById("latitude").innerHTML = latitude;
 document.getElementById("longitude").innerHTML = longitude;
 //START MAP
 var mymap = L.map('mapid').setView([latitude, longitude], 15);
+mymap.doubleClickZoom = false;
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BhY2V0b2FjZSIsImEiOiJjaXZmb2FrZG0wMTV1MnlvNnF2eHd5OXhqIn0.vs5YxulzCxYVvT4Fmhficg', {
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
 	maxZoom: 18,
 	id: 'your.mapbox.project.id',
 	accessToken: 'your.mapbox.public.access.token'
 }).addTo(mymap);
 
+
 //ENDMAP
 
 var marker = L.marker([latitude, longitude]).addTo(mymap);
-marker.bindPopup("<b>Lat: 37.333621</b><br> <b>Lon: -121.884899");
+marker.bindPopup("<b>Lat: </b>"+ latitude + "<br> <b>Lon: "+longitude + "<br> <b>Alt: "+altitude);
+
+mymap.on('click',onMapClick);
 
 
 function goToMarker(){
@@ -31,10 +35,23 @@ function goToRover(){
   mymap.setView([latitude, longitude]);
 }
 
-function removeMarker(){
-
+function removeMarker(e){
+	
 }
 
 function getMarkerPosition(){
 
+}
+
+function onMapClick(e) {
+    var tempLat = e.latlng.lat;
+	var tempLon = e.latlng.lng;
+	var tempAlt = e.latlng.alt;
+	document.getElementById("latitude").innerHTML = tempLat;
+	document.getElementById("longitude").innerHTML = tempLon;
+	document.getElementById("altitude").innerHTML = tempAlt;
+	var marker = L.marker([tempLat,tempLon,tempAlt]);
+	marker.bindPopup("<b>Lat: </b>"+ tempLat + "<br> <b>Lon: "+tempLon);
+	marker.addTo(mymap);
+	
 }
