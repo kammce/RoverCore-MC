5ed70dab8c3df9158195475c0dbe45716e18e4aa
Christian Timbol <christiantimbol@CT.local>
2017-04-07 16:20:46 -0700

    updeightz

--- interfaces/Arm/js/arm-interface.js
+++ interfaces/Arm/js/arm-interface.js
@@ -1,10 +1,14 @@
 /*
 =======================================================================================
-BEGIN excerpts from oldermishcontrol armInterface.js and rovercore-mc test interface
-Added to arm-interface.html:
-	<script type="text/javascript" src="js/testcontroller.js"></script>
-	<script type="text/javascript" src="js/testprimus.js"></script>
+BEGIN excerpts from oldermishcontrol armInterface.js 
 =======================================================================================
+*/
+
+
+/*
+=====================================================================================
+END excerpts from oldermishcontrol armInterface.js 
+=====================================================================================
 */
 
 var command = {
@@ -15,12 +19,6 @@ var command = {
 	"wrist_roll": 0,
 	"claw": 0,
 };
-
-/*
-=====================================================================================
-END excerpts from oldermishcontrol armInterface.js and rovercore-mc test interface js
-=====================================================================================
-*/
 
 //BEGIN Cam View
 	//Replace src
@@ -94,12 +92,13 @@ $("#RotundaSlider").slider({
 	    $( "#RotundaInputBox" ).val(ui.value);
 	    $( "#RotundaState" ).text(ui.value); //change this to converted degrees
 	    $( "#messages" ).html("Rotunda Changed!"); //make this more detailed and scrollable and add timestamp
-	    command.rotunda = ui.value;
+	    command.rotunda = ui.value; //command sent to 
 	}
 });
 
 $("#RotundaInputBox").change(function () {
 	$("#RotundaSlider").slider("value", parseInt(this.value));
+	command.rotunda = this.value; //rovercore-s command
 	$("#RotundaState").text(parseInt(this.value)); //change this to converted degrees
 	$( "#messages" ).html("Rotunda Changed!"); //make this more detailed and scrollable and add timestamp
 });
@@ -114,11 +113,13 @@ $("#Wrist_RollSlider").slider({
 	    $( "#Wrist_RollInputBox" ).val(ui.value);
 	    $( "#Wrist_RollState" ).text(ui.value); //change this to converted degrees
 		$( "#messages" ).html("Wrist_Roll Changed!"); //make this more detailed and scrollable and add timestamp
+		command.wrist_roll = ui.value; //rovercore-s command
 	}
 });
 
 	$("#Wrist_RollInputBox").change(function () {
 		$("#Wrist_RollSlider").slider("value", parseInt(this.value));
+		command.wrist_roll = this.value; //rovercore-s command
 		$("#Wrist_RollState").text(parseInt(this.value)); //change this to converted degrees
 		$( "#messages" ).html("Wrist_Roll Changed!"); //make this more detailed and scrollable and add timestamp
 });
@@ -133,11 +134,13 @@ $("#ShoulderSlider").slider({
 	    $( "#ShoulderInputBox" ).val(ui.value);
 	    $( "#ShoulderState" ).text(ui.value); //change this to converted degrees
 		$( "#messages" ).html("Shoulder Changed!"); //make this more detailed and scrollable and add timestamp
+		command.shoulder = ui.value; //rovercore-s command
 	}
 });
 
 	$("#ShoulderInputBox").change(function () {
 		$("#ShoulderSlider").slider("value", parseInt(this.value));
+		command.shoulder = this.value; //rovercore-s command
 		$("#ShoulderState").text(parseInt(this.value)); //change this to converted degrees
 		$( "#messages" ).html("Shoulder Changed!"); //make this more detailed and scrollable and add timestamp
 });
@@ -152,11 +155,13 @@ $("#ElbowSlider").slider({
       $( "#ElbowInputBox" ).val(ui.value);
       $( "#ElbowState" ).text(ui.value); //change this to converted degrees
       $( "#messages" ).html("Elbow Changed!"); //make this more detailed and scrollable and add timestamp
+      command.elbow = ui.value; //rovercore-s command
   }
 });
 
 	$("#ElbowInputBox").change(function () {
 		$("#ElbowSlider").slider("value", parseInt(this.value));
+		command.elbow = this.value; //rovercore-s command
 		$("#ElbowState").text(parseInt(this.value)); //change this to converted degrees
 		$( "#messages" ).html("Elbow Changed!"); //make this more detailed and scrollable and add timestamp
 });
@@ -171,11 +176,13 @@ $("#Wrist_PitchSlider").slider({
       $( "#Wrist_PitchInputBox" ).val(ui.value);
       $( "#Wrist_PitchState" ).text(ui.value); //change this to converted degrees
       $( "#messages" ).html("Wrist_Pitch Changed!"); //make this more detailed and scrollable and add timestamp
+      command.wrist_pitch = ui.value; //rovercore-s command
   }
 });
 
 	$("#Wrist_PitchInputBox").change(function () {
 		$("#Wrist_PitchSlider").slider("value", parseInt(this.value));
+		command.wrist_pitch = this.value; //rovercore-s command
 		$("#Wrist_PitchState").text(parseInt(this.value)); //change this to converted degrees
 		$( "#messages" ).html("Wrist_Pitch Changed!"); //make this more detailed and scrollable and add timestamp
 });
@@ -191,11 +198,13 @@ $("#ClawSlider").slider({
       $( "#ClawInputBox" ).val(ui.value);
       $( "#ClawState" ).text(ui.value); //change this to converted torque
       $( "#messages" ).html("Claw Changed!"); //make this more detailed and scrollable and add timestamp
+      command.claw = ui.value; //rovercore-s command
   }
 });
 
 	$("#ClawInputBox").change(function () {
 		$("#ClawSlider").slider("value", parseInt(this.value));
+		command.claw = this.value; //rovercore-s command
 		$("#ClawState").text(parseInt(this.value)); //change this to converted torque
 		$( "#messages" ).html("Claw Changed!"); //make this more detailed and scrollable and add timestamp
 });
@@ -214,11 +223,9 @@ $("#method0").click(function(){
 	$("#method5").removeClass('btn-info');
 	$("#method6").removeClass('btn-info');
     $( "#messages" ).html("Position changed to 0: Open Claw!"); //add timestamp
-	//insert appropriate slider values
-	//arm.claw = 2;
-	//SendPayload(arm);
 	$("#ClawSlider").slider('value', 2);
 	$("#ClawInputBox").val('2');
+	//command.claw = 2;
 	$("#ClawState").text(parseInt(this.value)); //change this to converted torque
 
 });
@@ -235,10 +242,9 @@ $("#method1").click(function(){
 	$("#method5").removeClass('btn-info');
 	$("#method6").removeClass('btn-info');
     $( "#messages" ).html("Position changed to 1: Close Claw!"); //add timestamp
-	//arm.claw = 1;
-	//SendPayload(arm);
 	$("#ClawSlider").slider('value', 1);
 	$("#ClawInputBox").val('1');
+	//command.claw = 1;
 	$("#ClawState").text(parseInt(this.value)); //change this to converted torque
 });
 
@@ -318,9 +324,10 @@ $("#method6").click(function(){
     $( "#messages" ).html("Position changed to 6: Reach Forward!"); //add timestamp
 });
 
-//Gamepad Mimic @12
+/*=======================
+Gamepad/Mimic Control @12
+=========================*/
 	var gamepadInfo = document.getElementById("gamepad-info");
-	var ball = document.getElementById("ball");
 	var start;
 	var a = 0;
 	var b = 0;
@@ -332,46 +339,40 @@ $("#method6").click(function(){
 	window.cancelRequestAnimationFrame;
 
 	window.addEventListener("gamepadconnected", function() {
-	var gp = navigator.getGamepads()[0];
-	gamepadInfo.innerHTML = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
-	    document.getElementById("gamepad-info").innerHTML = "MIMIC: On";
-
-	gameLoop();
+		var gp = navigator.getGamepads()[0];
+		document.getElementById("gamepad-info").innerHTML = "MIMIC: On";
+		gameLoop();
 	});
 
 	window.addEventListener("gamepaddisconnected", function() {
-	gamepadInfo.innerHTML = "Waiting for gamepad.";
-	rAFStop(start);
-	document.getElementById("gamepad-info").innerHTML = "MIMIC: Off";
-
+		rAFStop(start);
+		document.getElementById("gamepad-info").innerHTML = "MIMIC: Off";
 	});
 
 	if(!('GamepadEvent' in window)) {
-	// No gamepad events available, poll instead.
-	var interval = setInterval(pollGamepads, 500);
+		// No gamepad events available, poll instead.
+		var interval = setInterval(pollGamepads, 500);
 	}
 
 	function pollGamepads() {
-	var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
-	for (var i = 0; i < gamepads.length; i++) {
-	var gp = gamepads[i];
-	if(gp) {
-	  gamepadInfo.innerHTML = "Gamepad connected at index " + gp.index + ": " + gp.id + ". It has " + gp.buttons.length + " buttons and " + gp.axes.length + " axes.";
-	  gameLoop();
-	  clearInterval(interval);
+		var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
+		for (var i = 0; i < gamepads.length; i++) {
+			var gp = gamepads[i];
+			if(gp) {
+	 			gameLoop();
+	 			clearInterval(interval);
 			}
 		}
 	}
 
 	function buttonPressed(b) {
-	if (typeof(b) == "object") {
-		return b.pressed;
-	}
+		if (typeof(b) == "object") {
+			return b.pressed;
+		}
 		return b == 1.0;
 	}
 
-	/*BEGIN MIMIC VALUE CONVERSION */
-
+	//BEGIN MIMIC VALUE CONVERSION 
 	function gameLoop() {
 	var midBit = 512;
 
@@ -391,7 +392,7 @@ $("#method6").click(function(){
 	    return bit;
 	}
 
-	function posBitToDegree(x)
+	/*function posBitToDegree(x)
 	{
 	  var decimal
 	  if(x == 0)
@@ -423,10 +424,11 @@ $("#method6").click(function(){
 	  degree = Math.round(degree);
 	  return degree;
 	}
+	*/
 
-	/*END MIMIC VALUE CONVERSION */
+	//END MIMIC VALUE CONVERSION 
 
-	/* BEGIN MIMIC CONTROL */
+	//BEGIN MIMIC CONTROL
 
 	var gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads : []);
 	if (!gamepads)
@@ -435,16 +437,23 @@ $("#method6").click(function(){
 
 	if (buttonPressed(gp.buttons[0])) {
 	document.getElementById("buttonDisplay").innerHTML = "0: Open Claw";
+	//command.claw = ;
 
 	}
 
 	if (buttonPressed(gp.buttons[1])) {
 	document.getElementById("buttonDisplay").innerHTML = "1: Close Claw";
-
+	//command.claw = ;
 
 	}
 	if (buttonPressed(gp.buttons[2])) {
 	document.getElementById("buttonDisplay").innerHTML = "2: Drop POD";
+	//command.rotunda = ;
+	//command.shoulder = ;
+	//command.elbow = ;
+	//command.wrist_pitch = ;
+	//command.wrist_roll = ;
+	//command.claw = ;
 
 
 	}
@@ -467,50 +476,63 @@ $("#method6").click(function(){
 	}
 
 	if(gp.axes[0] != 0) {
-
 	 var baseRotationBit = controllerToBit(gp.axes[0]);
-	 var baseRotationDegree = posNegBitToDegree(baseRotationBit);
-	 document.getElementById("RotundaState").innerHTML = baseRotationDegree;
+	 document.getElementById("RotundaState").innerHTML = baseRotationBit;
+	 command.rotunda = baseRotationBit; 
+	 //var baseRotationDegree = posNegBitToDegree(baseRotationBit);
+	 //document.getElementById("RotundaState").innerHTML = baseRotationDegree;
 	}
 
 	if(gp.axes[1] != 0) {
 	var shoulderPitchBit = controllerToBit(gp.axes[1]);
-	 var shoulderPitchDegree = posBitToDegree(shoulderPitchBit);
-	 document.getElementById("ShoulderState").innerHTML = shoulderPitchDegree;
+	document.getElementById("ShoulderState").innerHTML = shoulderPitchBit;
+	command.shoulder = shoulderPitchBit;
+	//var shoulderPitchDegree = posBitToDegree(shoulderPitchBit);
+	//document.getElementById("ShoulderState").innerHTML = shoulderPitchDegree;
 	}
 
 	if(gp.axes[2] != 0) {
 	  var elbowPitchBit = controllerToBit(gp.axes[2]);
-	 var elbowPitchDegree = posBitToDegree(elbowPitchBit);
-	 document.getElementById("ElbowState").innerHTML = elbowPitchDegree;
+	 document.getElementById("ElbowState").innerHTML = elbowPitchBit;
+	 command.elbow = elbowPitchBit;
+	 //var elbowPitchDegree = posBitToDegree(elbowPitchBit);
+	 //document.getElementById("ElbowState").innerHTML = elbowPitchDegree;
 	}
 
 	if(gp.axes[3] != 0) {
 	  var torqueBit = controllerToBit(gp.axes[3]);
-	 var torqueDegree = posBitToDegree(torqueBit);
-	 document.getElementById("ClawState").innerHTML = torqueDegree;
+	 document.getElementById("ClawState").innerHTML = torqueBit;
+	 command.claw = torqueBit;
+	 //var torqueDegree = posBitToDegree(torqueBit);
+	 //document.getElementById("ClawState").innerHTML = torqueDegree;
 	}
 
 
 	if(gp.axes[4] != 0) {
 	  var wristPitchBit = controllerToBit(gp.axes[4]);
-	 var wristPitchDegree = posBitToDegree(wristPitchBit);
-	 document.getElementById("Wrist_PitchState").innerHTML = wristPitchDegree;
+	 document.getElementById("Wrist_PitchState").innerHTML = wristPitchBit;
+	 command.wrist_pitch = wristPitchBit;
+	 //var wristPitchDegree = posBitToDegree(wristPitchBit);
+	 //document.getElementById("Wrist_PitchState").innerHTML = wristPitchDegree;
 	}
 
 
 	if(gp.axes[5] != 0) {
 	 var wristRotationBit = controllerToBit(gp.axes[5]);
-	 var wristRotationDegree = posNegBitToDegree(wristRotationBit);
-	 document.getElementById("Wrist_RollState").innerHTML = wristRotationDegree;
+	 document.getElementById("Wrist_RollState").innerHTML = wristRotationBit;
+	 command.wrist_rotation = wristRotationBit;
+	 //var wristRotationDegree = posNegBitToDegree(wristRotationBit);
+	 //document.getElementById("Wrist_RollState").innerHTML = wristRotationDegree;
 	}
 
 
-	ball.style.left = a*2 + "px";
-	ball.style.top = b*2 + "px";
 	var start = rAF(gameLoop);
 };
 
+
+/*=====================
+rovercore-s connection
+======================*/
 var LobeAssignmentInterval = setInterval(function()
 {
 	if(Connection.state === Connection.CONNECTED)
@@ -526,11 +548,12 @@ var LobeAssignmentInterval = setInterval(function()
 
 var SendToRoverCoreS = setInterval(() =>
 {
-	var payload = {
-		target: target,
-		command: command
-	};
-	primus.write(payload);
+	if(Connection.state === Connection.CONNECTED)
+	{	
+		var payload = {
+			target: 'Arm',
+			command: command
+		};
+		primus.write(payload);
+	}
 }, 100);
-
-
