58081093158db1ac9f6d6088bb84b561ee5c1c68
khalil estell <khalil.estell@sjsu.edu>
2017-05-28 19:38:07 -0700

    Adding Battery level stuff. Battery level does not indicate anything, but can shut off battery

--- interfaces/Captain/js/PowerSystem.js
+++ interfaces/Captain/js/PowerSystem.js
@@ -60,6 +60,26 @@ document.querySelectorAll("#Battery > button").forEach(function(obj, index, arra
 {
 	array[index].onclick = function()
 	{
-		console.log(array[index].id);
+		var value = parseInt(array[index].getAttribute("data-value"));
+		var id = array[index].id;
+		if(isNaN(value))
+		{
+			console.warn("did not parse");
+		}
+		else
+		{
+			var invert = (value == 1) ? 0 : 1;
+			var payload = {
+				target: "PowerSystems",
+				command: { }
+			};
+			payload.command[id] = invert;
+			primus.write(payload);
+			array[index].setAttribute("data-value", invert);
+			console.log(payload);
+			console.log(array[index].id);
+			console.log(value);
+			console.log(invert);
+		}
 	}
 });
