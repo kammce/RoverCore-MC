69720a44f09a38a3b0968643eb99e51a7f21ba06
Rolando Javier <rjavier441@gmail.com>
2017-05-31 16:19:49 -0700

    Merging changes from rj/Tracker (newly configured interface) into branch Tracker

--- interfaces/Tracker/script.js
+++ interfaces/Tracker/script.js
@@ -1,11 +1,32 @@
+// script.js
+/* begin: model snapshot pane */
 var ModelInterval = setInterval(function()
 {
   if(Connection.state === Connection.CONNECTED)
   {
+    // update model snapshot pane
     ModelJSONEditor.set(model);
-  }
-}, 200);
 
+    // update interface values
+    if (typeof model.Tracker !== "undefined") {
+      $("#recordedDistance").html(model.Tracker.value.distance);
+      $("#zoomFeedback").html(model.Tracker.value.zoom);
+      $("#yawPosFeedback").html(model.Tracker.value.yaw.angle);
+      $("#pitchPosFeedback").html(model.Tracker.value.pitch.angle);
+      $("#activeCameraFeedback").html(model.Tracker.value.activeCamera);
+    } else {
+      $("#recordedDistance").html("--No Data--");
+      $("#zoomFeedback").html("--No Data--");
+      $("#yawPosFeedback").html("--No Data--");
+      $("#pitchPosFeedback").html("--No Data--");
+      $("#activeCameraFeedback").html("--No Data--");
+    }
+    // console.log(JSON.stringify(model));
+  }
+}, 100);
+/* end: model snapshot pane */
+
+/* begin: lobe console updater */
 var prev_message = messages;
 var MessageInterval = setInterval(function()
 {
@@ -15,6 +36,7 @@ var MessageInterval = setInterval(function()
     prev_message = messages;
   }
 }, 100);
+/* end: lobe console updater */
 
 var options = {
     mode: 'code',
@@ -65,13 +87,14 @@ function SendPayload(json)
   }
 }
 
+/* Warning: Map needs to be locally saved; without it, using internet is necessary (but not feasible) */
 var mymap = L.map('mapid').setView([38.4064, -110.7919], 15);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3BhY2V0b2FjZSIsImEiOiJjaXZmb2FrZG0wMTV1MnlvNnF2eHd5OXhqIn0.vs5YxulzCxYVvT4Fmhficg', {
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-	maxZoom: 18,
-	id: 'your.mapbox.project.id',
-	accessToken: 'your.mapbox.public.access.token'
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'your.mapbox.project.id',
+  accessToken: 'your.mapbox.public.access.token'
 }).addTo(mymap);
 
 //zoom
@@ -79,10 +102,16 @@ function Slides(sliderValue){
   var inp = document.getElementById('ZoomSlide');
   console.log(inp.value);
   document.getElementById('ZoomCurrentValue').textContent = inp.value;
-  inp.addEventListener("mousemove", function() {
-    document.getElementById('ZoomCurrentValue').textContent = inp.value;
-  });
+  zoomSlideUnclick();
 };
+
+function zoomSlideUnclick(){
+  var k = $("#ZoomSlide").val();
+  console.log("Changing Zoom to: " + k);
+
+  command.zoom = Number(k);
+  SendPayload(command);
+}
 
 function setNewValZoom(){
   var zoomVal = document.getElementById("zoomInp").value;
@@ -131,6 +160,9 @@ function YPSlides(sliderValue){
     document.getElementById('YawPosCurrentValue').textContent = inp.value;
   });
   command.yaw.angle = Number(inp.value);
+
+  console.log("Updated yaw to: " + inp.value);
+  YPSlideUnclick();
 };
 
 function YPSlideUnclick(){
@@ -189,6 +221,8 @@ function PPSlides(sliderValue){
     document.getElementById('PitchPosCurrentValue').textContent = inp.value;
   });
   command.pitch.angle = Number(inp.value);
+  console.log("updating pitch to: " + inp.value);
+  PPSlideUnclick();
 };
 
 function PPSlideUnclick(){
@@ -217,42 +251,68 @@ function PPEntZoom(val, e){
 
 //Lidar 
 function LidarClick(){
-  console.log("Lidar clicked");
-  var button = document.getElementById("LidarEnable");
-  if(button.classList.contains("disabled")){
-    button.classList.add('enabled');
-    button.classList.remove('disabled');
-    button.textContent = 'Enabled';
-    console.log('Lidar Enabled');
-    command.lidar = 1;
-  }
-  else{
-    button.classList.add('disabled');
-    button.classList.remove('enabled');
-    button.textContent = 'Disabled';
-    console.log('Lidar Disabled');
-    command.lidar = 0;
+  // console.log("Lidar clicked");
+  // var button = document.getElementById("LidarEnable");
+  // if(button.classList.contains("disabled")){
+  //   button.classList.add('enabled');
+  //   button.classList.remove('disabled');
+  //   button.textContent = 'Enabled';
+  //   console.log('Lidar Enabled');
+  //   command.lidar = 1;
+  // }
+  // else{
+  //   button.classList.add('disabled');
+  //   button.classList.remove('enabled');
+  //   button.textContent = 'Disabled';
+  //   console.log('Lidar Disabled');
+  //   command.lidar = 0;
+  // }
+  if ($("#LidarEnable").hasClass("disabled")) {
+    $("#LidarEnable").removeClass("disabled");
+    $("#LidarEnable").addClass("enabled");
+    $("#LidarEnable").html("Enabled");
+    command.lidar = true;
+    console.log("LidarClick: Lidar Enabled");
+  } else {
+    $("#LidarEnable").removeClass("enabled");
+    $("#LidarEnable").addClass("disabled");
+    $("#LidarEnable").html("Disabled");
+    command.lidar = false;
+    console.log("LidarClick: Lidar Disabled");
   }
   SendPayload(command);
 };
 
 //Camera Switch  (disabled = camera B, enabled = camera A)
 function CamSwitch(){
-  console.log("Camera Switched");
-  var button = document.getElementById("CamSwitch");
-  if(button.classList.contains("disabled")){
-    button.classList.add('enabled');
-    button.classList.remove('disabled');
-    button.textContent = 'Switch to Camera A';
-    console.log('Camera B');
+  // console.log("Camera Switched");
+  // var button = document.getElementById("CamSwitch");
+  // if(button.classList.contains("disabled")){
+  //   button.classList.add('enabled');
+  //   button.classList.remove('disabled');
+  //   button.textContent = 'Switch to Camera A';
+  //   console.log('Camera B');
+  //   command.activeCamera = 1;
+  // }
+  // else{
+  //   button.classList.add('disabled');
+  //   button.classList.remove('enabled');
+  //   button.textContent = 'Switch to Camera B';
+  //   console.log('Camera A');
+  //   command.activeCamera = 0;
+  // }
+  if ($("#CamSwitch").hasClass("disabled")) {
+    $("#CamSwitch").removeClass("disabled");
+    $("#CamSwitch").addClass("enabled");
+    $("#CamSwitch").html("Camera B");
     command.activeCamera = 1;
-  }
-  else{
-    button.classList.add('disabled');
-    button.classList.remove('enabled');
-    button.textContent = 'Switch to Camera B';
-    console.log('Camera A');
+    console.log("CamSwitch: Feed from Camera B");
+  } else {
+    $("#CamSwitch").removeClass("enabled");
+    $("#CamSwitch").addClass("disabled");
+    $("#CamSwitch").html("Camera A");
     command.activeCamera = 0;
+    console.log("CamSwitch: Switched to Camera A");
   }
   SendPayload(command);
 };
